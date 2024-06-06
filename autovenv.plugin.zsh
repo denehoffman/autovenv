f2d3ac0c670e69f24e60b9cf8efd791a8bbd57fe
denehoffman <denehoffman@me.com>
2024-06-05 22:27:42 -0400

    feat: adds oh-my-zsh plugin for zsh

---
+++ autovenv.plugin.zsh
@@ -0,0 +1,25 @@
+find_and_activate_venv() {
+    local dir="$1"
+    while [[ "$dir" != "/" && "$dir" != "$HOME" ]]; do
+        venv_activate="$dir/.venv/bin/activate"
+        if [[ -f "$venv_activate" ]]; then
+            source "$venv_activate"
+            return 0
+        fi
+        dir=$(dirname "$dir")
+    done
+    return 1
+}
+on_cd() {
+    emulate -L zsh
+    if [[ -n "$VIRTUAL_ENV" ]]; then
+        virtual_env_parent=$(dirname "$VIRTUAL_ENV")
+        if [[ "$PWD" != "$virtual_env_parent"* ]]; then
+            deactivate
+            find_and_activate_venv "$PWD"
+        fi
+    else
+        find_and_activate_venv "$PWD"
+    fi
+}
+chpwd_functions+=(on_cd)
